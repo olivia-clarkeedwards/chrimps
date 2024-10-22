a9eba2f0aad0272c35b1b6433eb4387229cc0b68
Ryan Kendrick <ryan.kendrick93@gmail.com>
2024-10-22 21:28:21 +1300

    begin work on container elements

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -4,9 +4,11 @@ import React from "react";
 
 export default function Page() {
   return (
-    <div className="flex lg:flex-row flex-col">
-      <div className="basis-3/5">Game</div>
-      <div className="basis-2/5">Combat</div>
+    <div className="flex h-screen">
+      <div className="flex w-screen self-center h-4/6 lg:flex-row flex-col">
+        <div className="basis-3/5 h-[66svh] bg-gradient-to-b from-amber-300 to-amber-950">Game</div>
+        <div className="basis-2/5 h-[66svh] bg-slate-500">Combat</div>
+      </div>
     </div>
   );
 }
