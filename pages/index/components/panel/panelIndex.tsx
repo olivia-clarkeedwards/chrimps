279f1fd19600c6b3828eab8470e6fdd3d7e2e2a7
Ryan Kendrick <ryan.kendrick93@gmail.com>
2024-11-05 20:34:14 +1300

    fix gold coin position (for now)

--- pages/index/components/panel/panelIndex.tsx
+++ pages/index/components/panel/panelIndex.tsx
@@ -7,11 +7,11 @@ export default function Panel() {
   const gold = useAppSelector(selectGold)
 
   return (
-    <div className="flex flex-col basis-3/5 bg-gradient-to-b from-amber-300 to-amber-950">
-      <div id="gold-cont" className="flex basis-1/6 relative">
-        <div className="absolute flex items-center gap-1 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <img className="w-1/2 " src="/icons/coin.png" alt="gold coin" />
-          <div className="text-3xl">{gold}</div>
+    <div className="flex flex-col basis-3/5 min-h-[66svh] bg-gradient-to-b from-amber-300 to-amber-950">
+      <div id="gold-cont" className="flex flex-col items-center basis-1/6 relative">
+        <div className="flex absolute items-center gap-1 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <img className="pl-20" src="/icons/coin.png" alt="gold coin" />
+          <span className="text-3xl min-w-[9ch] text-left">{gold}</span>
         </div>
       </div>
       <UpgradeIndex />
