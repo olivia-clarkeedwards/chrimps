b7365818791f076271766d7c762f092b6a66b6a4
Ryan Kendrick <ryan.kendrick93@gmail.com>
2024-11-09 12:12:32 +1300

    refine stage visualiser layout

--- pages/index/components/combat/zoneVisualiser.tsx
+++ pages/index/components/combat/zoneVisualiser.tsx
@@ -9,17 +9,19 @@ export default function ZoneVisualiser() {
   const currentStage = useAppSelector(selectZoneNumber)
 
   return (
-    <div className="flex absolute bottom-0 w-[40rem] m-2 flex-wrap-reverse content-start border-2 border-gray-300 box-content">
-      {stages.map((stageNumber) => (
-        <div
-          key={stageNumber}
-          className="relative h-8 w-16 border-2 border-gray-300 flex items-center justify-center text-sm">
+    <div className="flex items-end">
+      <div className="flex w-[20rem] md:w-[40rem] lg:w-[40rem] md:m-2 flex-wrap-reverse content-start border-2 border-gray-300 box-content">
+        {stages.map((stageNumber) => (
           <div
-            className={clsx("absolute w-full h-full border-2", stageNumber === currentStage && "border-yellow-400")}
-          />
-          {stageNumber}
-        </div>
-      ))}
+            key={stageNumber}
+            className="relative h-8 w-16 border-2 border-gray-300 flex items-center justify-center text-sm">
+            <div
+              className={clsx("absolute w-full h-full border-2", stageNumber === currentStage && "border-yellow-400")}
+            />
+            {stageNumber}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
