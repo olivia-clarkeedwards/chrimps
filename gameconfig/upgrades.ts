848d74fa66002f89436fbccacb5ad32b2bd32a69
Ryan Kendrick <ryan.kendrick93@gmail.com>
2024-11-17 21:30:31 +1300

    use more linear scaling for levelup

--- gameconfig/upgrades.ts
+++ gameconfig/upgrades.ts
@@ -5,13 +5,21 @@ export const UPGRADE_CONFIG: UpgradeConfig = {
     elementId: "click-multi",
     costKey: "clickMultiCosts",
     costs: [100, 400, 1000],
-    levelUpCost: (currentLevel) => Math.floor(10 * Math.pow(1.1, currentLevel) - 1),
+    levelUpCost: (currentLevel) => {
+      const base = 10
+      const growthRate = 1.1
+      return Math.floor(base * (1 + Math.log10(currentLevel)) * Math.pow(growthRate, currentLevel) - 1)
+    },
   },
   dot: {
     elementId: "dot-multi",
     costKey: "dotMultiCosts",
     costs: [5000, 10000, 25000],
-    levelUpCost: (currentLevel) => Math.floor(499 + 200 * currentLevel + Math.pow(1.2, currentLevel)),
+    levelUpCost: (currentLevel) => {
+      const base = 500
+      const growthRate = 1.1
+      return Math.floor(base * (1 + Math.log10(currentLevel + 1)) * Math.pow(growthRate, currentLevel))
+    },
   },
   calcMultiCost: function (upgradeName, upgradeCount) {
     const costs = {
