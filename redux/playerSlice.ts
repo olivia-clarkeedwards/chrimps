59964e3b71e9d8b213f1ba95721b554fee946d38
Ryan Kendrick <ryan.kendrick93@gmail.com>
2024-11-23 18:19:06 +1300

    touch up levelup button

--- redux/playerSlice.ts
+++ redux/playerSlice.ts
@@ -7,7 +7,7 @@ import { PlayerState } from "../models/state"
 const initialState: PlayerState = {
   clickLevel: 1,
   clickMultiUpgradeCount: 0,
-  gold: 10000000,
+  gold: 10,
   dotLevel: 0,
   dotMultiUpgradeCount: 0,
 }
