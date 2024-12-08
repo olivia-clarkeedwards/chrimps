15781599ee348a448bc2399a77b7b28767f54c85
Ryan Kendrick <ryan.kendrick93@gmail.com>
2024-12-08 21:53:59 +1300

    fix farm toggle fade-in

--- components/combat/farmToggle.tsx
+++ components/combat/farmToggle.tsx
@@ -28,12 +28,15 @@ export default function FarmToggle() {
 
   return (
     // Todo: migrate to tailwind 4.0 and use radial gradients for circular buttons
-    <div className="absolute flex items-center justify-center w-10 h-10 right-2 top-1 bg-gradient-to-tr from-yellow-400/20 to-yellow-400 rounded-full">
+    <div
+      className={clsx(
+        "absolute flex items-center justify-center w-10 h-10 right-2 top-1 bg-gradient-to-tr from-yellow-400/20 to-yellow-400 rounded-full opacity-0",
+        !hasTransitioned ? "transition-opacity duration-1000" : "transition-none",
+        currentZone > 4 && "opacity-100",
+      )}>
       <div
         className={clsx(
-          "w-8 h-8 -rotate-45 border border-2 border-[3px] rounded-full opacity-0",
-          !hasTransitioned ? "transition-opacity duration-1000" : "transition-none",
-          currentZone > 4 && "opacity-100",
+          "w-8 h-8 -rotate-45 border border-2 border-[3px] rounded-full",
           isFarming ? "fill-gray-700 border-gray-700 opacity-60" : "fill-white border-white",
         )}
         onClick={handleFarmToggle}>
