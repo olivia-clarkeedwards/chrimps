889a9884b0477c5bad38ffba6bb17e7cce8a449a
Olivia <oliviaclarkeedwards@gmail.com>
2024-12-10 21:23:18 +1300

    style buttons black

--- components/nav/mainNavigationMenu.tsx
+++ components/nav/mainNavigationMenu.tsx
@@ -1,15 +1,20 @@
 export function MainNavigationMenu() {
   return (
-    <div className="flex flex-wrap justify-between bg-gray-800 text-white p-4 h-full">
-      <div className="flex">
-        <div className="p-2">Home</div>
-        <div className="p-2">Combat</div>
-        <div className="p-2">Inventory</div>
-        <div className="p-2">Settings</div>
+    <div className="flex flex-wrap justify-between bg-gray-900 text-white p-4 h-full">
+      <div className="flex gap-2">
+        <button
+          className="m-2 py-4 px-8 text-center uppercase transition duration-500 bg-gradient-to-r from-red-500 via-orange-400 to-red-500 text-white rounded-lg block border-0 font-bold shadow-lg cursor-pointer select-none hover:bg-right-center active:transform active:scale-95"
+          role="button">
+          Button 87
+        </button>
+        <button className="py-2 px-4 rounded-xl hover:shadow-slate-600/70 hover:shadow-lg">Home</button>
+        <button className="py-2 px-4 rounded-xl hover:shadow-slate-600/70 hover:shadow-lg">Combat</button>
+        <button className="py-2 px-4 rounded-xl hover:shadow-slate-600/70 hover:shadow-lg">Inventory</button>
+        <button className="py-2 px-4 rounded-xl hover:shadow-slate-600/70 hover:shadow-lg">Settings</button>
       </div>
       <div className="flex">
-        <div className="p-2">Save</div>
-        <div className="p-2">Load</div>
+        <button className="py-2 px-4 rounded-xl hover:shadow-slate-600/70 hover:shadow-lg">Save</button>
+        <button className="py-2 px-4 rounded-xl hover:shadow-slate-600/70 hover:shadow-lg">Load</button>
       </div>
     </div>
   )
