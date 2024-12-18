c97e2b960877665c72c1edfba224d3240e57197b
Ryan Kendrick <ryan.kendrick93@gmail.com>
2024-12-19 07:04:05 +1300

    move nav to bottom at sm breakpoint

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   return (
     <React.StrictMode>
       <Provider store={store}>
-        <div className="cursor-hand flex-col min-h-screen lg:h-screen select-none bg-gradient-to-b from-purple-800 via-purple-900 to-violet-950 overflow-hidden">
+        <div className="cursor-hand flex flex-col-reverse md:flex-col min-h-screen lg:h-screen select-none bg-gradient-to-b from-purple-800 via-purple-900 to-violet-950 overflow-hidden">
           <div className="lg:min-h-[5rem] lg:h-[10%] shrink-0">
             <Navigation />
           </div>
