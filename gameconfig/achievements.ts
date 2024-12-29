5b414847c104c9d34ed97fda723d2f4f55570eb8
Ryan Kendrick <ryan.kendrick93@gmail.com>
2024-12-29 20:39:53 +1300

    add achievement config file

---
+++ gameconfig/achievements.ts
@@ -0,0 +1,122 @@
+export const ACHIEVEMENT_CONFIG = {
+  zone: {
+    completion: [
+      {
+        id: "zone-completion.1",
+        title: "First Steps",
+        description: "Complete zone 10",
+        difficulty: "beginner",
+        condition: 10,
+      },
+    ],
+  },
+  click: {
+    count: [
+      {
+        id: "click-count.1",
+        title: "Clicker",
+        description: "Click monsters 100 times",
+        difficulty: "beginner",
+        condition: 100,
+      },
+      {
+        id: "click-count.2",
+        title: "Mouse User",
+        description: "Click monsters 1000 times",
+        difficulty: "beginner",
+        condition: 1000,
+      },
+      {
+        id: "click-count.3",
+        title: "Certified Mouse User",
+        description: "Click monsters 5000 times",
+        difficulty: "novice",
+        condition: 5000,
+      },
+      {
+        id: "click-count.3",
+        title: "Certificate in Applied Mouse Usership: Full Range-Of-Motion Clicker",
+        description: "Click monsters 10000 times",
+        difficulty: "novice",
+        condition: 10000,
+      },
+    ],
+    damage: [
+        {
+            id: "click-damage.1",
+            title: "",
+            description:,
+            difficulty:,
+            condition:,
+        }
+        {
+            id: "",
+            title: "",
+            description: "",
+            difficulty: "",
+            condition: ,
+        }
+    ]
+  },
+
+//   clickCount: {
+//     id: "clickCount",
+//     title: "Clicker",
+//     description: "Click the monster 100 times",
+//     goal: 100,
+//   },
+//   totalClickDamage: {
+//     id: "totalClickDamage",
+//     title: "Damage Dealer",
+//     description: "Deal 1000 damage",
+//     goal: 1000,
+//   },
+//   totalDotDamage: {
+//     id: "totalDotDamage",
+//     title: "Dot Dealer",
+//     description: "Deal 1000 damage over time",
+//     goal: 1000,
+//   },
+//   killCount: {
+//     id: "killCount",
+//     title: "Killer",
+//     description: "Kill 100 monsters",
+//     goal: 100,
+//   },
+//   farmZonesCompleted: {
+//     id: "farmZonesCompleted",
+//     title: "Farmer",
+//     description: "Complete 10 farm zones",
+//     goal: 10,
+//   },
+//   totalZonesCompleted: {
+//     id: "totalZonesCompleted",
+//     title: "Explorer",
+//     description: "Complete 10 zones",
+//     goal: 10,
+//   },
+//   highestZoneEver: {
+//     id: "highestZoneEver",
+//     title: "Adventurer",
+//     description: "Reach zone 10",
+//     goal: 10,
+//   },
+//   prestigeCount: {
+//     id: "prestigeCount",
+//     title: "Prestiger",
+//     description: "Prestige 1 time",
+//     goal: 1,
+//   },
+//   zoneTenCompleted: {
+//     id: "zoneTenCompleted",
+//     title: "Zone Ten",
+//     description: "Complete zone 10",
+//     goal: 1,
+//   },
+//   highestZone: {
+//     id: "highestZone",
+//     title: "High Roller",
+//     description: "Reach zone 10 in a single run",
+//     goal: 10,
+//   },
+}
