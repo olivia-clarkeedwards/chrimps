924bd6b8c620411a031e7e02cf877370966a0667
Ryan Kendrick <ryan.kendrick93@gmail.com>
2025-01-03 17:43:22 +1300

    reinstall vike

--- pages/+config.ts
+++ pages/+config.ts
@@ -1,16 +1,15 @@
 import vikeReact from "vike-react/config"
 import type { Config } from "vike/types"
-import logoUrl from "../assets/logo.svg"
 
 export default {
   title: "Slime Ascendant",
   description: "The Incremental Adventure",
-  image: logoUrl,
-  favicon: logoUrl,
+  image: "icons/logo.svg",
+  favicon: "icons/logo.svg",
   ssr: false,
   prerender: false,
   passToClient: ["user"],
-  extends: "vike-react/config",
+  extends: vikeReact,
   stream: false,
   clientRouting: true,
 } satisfies Config
