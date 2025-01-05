2968aa9697739272a7661410a63830945a68e4fd
Ryan Kendrick <ryan.kendrick93@gmail.com>
2025-01-06 07:18:47 +1300

    replace slime placeholder

--- gameconfig/monster.ts
+++ gameconfig/monster.ts
@@ -1,6 +1,6 @@
 import { BaseEnemy, Enemy, MonsterType, BaseMonsterConfig, EnemyState } from "../models/monsters"
 import { ZONE_CONFIG } from "./zone"
-import slimeURL from "../assets/monsters/ph-slime.webp"
+import slimeURL from "../assets/monsters/slime.webp"
 import wormURL from "../assets/monsters/ph-worm.webp"
 import cacodemonURL from "../assets/monsters/ph-cacodemon.webp"
 import yetiURL from "../assets/monsters/ph-yeti.webp"
